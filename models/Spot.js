02f05cf70675a58c2a5d08ba1162c356b3bfa9ad
Julia Ramos <juliaramosguedes@gmail.com>
2019-09-12 20:54:33 -0300

    feat: cria modelos de banco de dados

---
+++ models/Spot.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const spotSchema = Schema(
+  {
+    authorId: { type: String, required: true },
+    name: { type: String, required: true },
+    phone: String,
+    address: { type: String, required: true },
+    coord: {
+      lat: String,
+      lng: String,
+    },
+    // zipcode: String,
+    vegCategory: {
+      type: String,
+      enum: ['Vegan', 'Vegetariano', 'Vegan-Friendly'],
+      required: true,
+    },
+    spotCategory: {
+      type: String,
+      enum: ['Restaurante', 'Loja'],
+      required: true,
+    },
+    rating: { type: Number, required: true },
+    googlePlaceId: { type: String },
+    weekday: { type: String, required: true },
+    photos: Array,
+    googleReviews: Array,
+    price: Number,
+  },
+);
+
+const Spot = mongoose.model('Spot', spotSchema);
+
+module.exports = Spot;
