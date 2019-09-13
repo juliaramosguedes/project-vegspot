a3752a3f4bf707745a655a96c0079c4c70cee7d1
danyamashita <dan.yamashita@gmail.com>
2019-09-13 20:47:52 -0300

    maps rev 02

--- public/javascript/maps.js
+++ public/javascript/maps.js
@@ -16,10 +16,14 @@ if (navigator.geolocation) {
 }
 
 function initMap() {
+  if (!pos) {
+    pos = { lat: -23.5506507, lng: -46.6333824 };
+  }
+
   map = new google.maps.Map(document.getElementById('map'), {
     // center: { lat: -23.5506507, lng: -46.6333824 },
     center: pos,
-    zoom: 12,
+    zoom: 17,
   });
 
   infoWindow = new google.maps.InfoWindow();
@@ -53,8 +57,44 @@ function addMarker(coords) {
     content: contentString,
   });
 
-  marker.addListener('click', () => {
+  marker.addListener('mouseover', () => {
     infowindow.open(map, marker);
+  });
+
+  marker.addListener('click', () => {
+    console.log(infoWindow.content)
+  });
+
+  marker.addListener('mouseout', () => {
+    infowindow.close();
+  });
+}
+
+function addMarkerPlace(place) {
+  const coords = place.geometry.location;
+  console.log(place.name, place.formatted_address)
+  const contentString = `
+  <div>${place.name}</div>
+  <div>${place.formatted_address}</div>`;
+  const marker = new google.maps.Marker({
+    position: coords,
+    map,
+    icon: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
+  });
+
+  infoWindow = new google.maps.InfoWindow();
+  infoWindow.setContent(contentString);
+
+  marker.addListener('mouseover', () => {
+    infoWindow.open(map, marker);
+  });
+
+  marker.addListener('click', () => {
+    console.log(place)
+  });
+
+  marker.addListener('mouseout', () => {
+    infoWindow.close();
   });
 }
 
@@ -74,33 +114,51 @@ function geocode(location) {
       document.getElementById('formatted-address').innerHTML += `${formattedAddress}`;
       addMarker(coord);
       console.log('geocode', coord, formattedAddress, placeId);
+      placeDetails(placeId);
     })
     .catch((error) => {
       console.log(error);
     });
 }
-placeDetails();
 
-// function placeDetails() {
-//   axios.get('https://maps.googleapis.com/maps/api/place/details/json', {
-//     params: {
-//       place_id: 'ChIJW5hIjUZZzpQRDJtd2P4nT2k',
-//       key: 'AIzaSyAFJywZr8chFDtxNX5tPazJiwOvUr6pQCQ',
-//     },
-//   })
-//     .then((detail) => {
-//       console.log('detail', detail);
-//     })
-//     .catch((error) => {
-//       console.log(error)
-//     });
-// }
+function findPlaces(text) {
+  const request = {
+    // location: '',
+    // radius: '',
+    query: text,
+  };
 
-function placeDetails() {
-  axios.get('https://maps.googleapis.com/maps/api/place/details/json?place_id=ChIJW5hIjUZZzpQRDJtd2P4nT2k&key=AIzaSyAFJywZr8chFDtxNX5tPazJiwOvUr6pQCQ')
-    .then((detail) => {
-      console.log('detail', detail);
-    });
+  service = new google.maps.places.PlacesService(map);
+  service.textSearch(request, callback);
+  let bounds = new google.maps.LatLngBounds()
+
+  function callback(places, status) {
+    console.log('places', places);
+    if (status == google.maps.places.PlacesServiceStatus.OK) {
+      for (let i = 0; i < places.length; i++) {
+        addMarkerPlace(places[i]);
+        bounds.extend(places[i].geometry.location)
+      }
+      map.fitBounds(bounds)
+    }
+  }
+}
+
+
+function placeDetails(id) {
+  const request = {
+    placeId: id,
+    // fields: ['name', 'rating', 'formatted_phone_number', 'geometry'],
+  };
+
+  service = new google.maps.places.PlacesService(map);
+  service.getDetails(request, callback);
+
+  function callback(place, status) {
+    if (status == google.maps.places.PlacesServiceStatus.OK) {
+      console.log(place);
+    }
+  }
 }
 
 
@@ -109,6 +167,7 @@ document.getElementById('button').onclick = function (event) {
   document.getElementById('formatted-address').innerHTML = '';
   const address = document.getElementById('address').value;
   console.log('address', address);
+  findPlaces(address)
   geocode(address);
   document.getElementById('address').value = '';
 };
