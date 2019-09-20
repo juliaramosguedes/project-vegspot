01265d4033c010cec168a390370206cc6a0b0afd
Julia Ramos <juliaramosguedes@gmail.com>
2019-09-20 20:08:51 -0300

    create search and spot routes, searh and spot views, seeds for user and spots

--- routes/private/spot-routes.js
+++ routes/private/spot-routes.js
@@ -1,11 +1,38 @@
 const passport = require('passport');
 const express = require('express');
 
+const Spot = require('../../models/Spot');
 const router = express.Router();
 const ensureLogin = require('connect-ensure-login');
 
 const checkRoles = require('../../middlewares/passport-middleware');
 
 const checkAchiever  = checkRoles('achiever');
+
+router.post('/search', async (req, res) => {
+  const { searchSpot } = req.body;
+  try {
+    const spots = await Spot.find({ name: searchSpot });
+    res.render('public/search', { spots });
+  } catch (error) {
+    console.log(error);
+    res.redirect('/');
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const spot = await Spot.findById(id);
+    if (req.user.role === 'achiever') {
+      res.render('private/spot-adm', spot);
+    } else {
+      res.render('public/spot-profile', spot);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
+
 
 module.exports = router
