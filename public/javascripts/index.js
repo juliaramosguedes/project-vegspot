a2a4a983d526bd2fda26826fd8a811bd8d54e4fd
danyamashita <dan.yamashita@gmail.com>
2019-10-03 08:12:41 -0300

    near places with range search

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,23 +1,43 @@
 window.onload = async () => {
+  let maxDistance
   const url = 'http://localhost:3000';
   // const url = `http://localhost:${process.env.PORT}`
 
-  async function getNearPlaces() {
+  async function getNearPlaces(maxDistance) {
     const position = [pos.lng, pos.lat];
-    const maxDistance = 8000;
     const results = await axios.post(`${url}/`, { position, maxDistance });
     return results;
   }
 
-  const places = await getNearPlaces();
-  console.log('placessss', places.data, places.data.coord);
-  places.data.forEach((place, index) => {
-    let {coord} = place;
-    coord = {
-      lat: coord[1],
-      lng: coord[0],
-    };
-    places.data[index].coord = coord;
-  });
-  addMarker(places.data);
+  async function loadNearPlaces() {
+    maxDistance = Number(document.getElementById('nearRange').value);
+    const places = await getNearPlaces(maxDistance);
+    console.log('placessss', places.data);
+    console.log('maxdistance loading page', maxDistance, typeof(maxDistance))
+    if (places.data.length) {
+      places.data.forEach((place, index) => {
+        let {coord} = place;
+        coord = {
+          lat: coord[1],
+          lng: coord[0],
+        };
+        places.data[index].coord = coord;
+      });
+      console.log('placessss', places.data);
+      addMarker(places.data);
+    } else {
+      document.getElementById("nearPlacesMessage").innerHTML = `
+      Nao foram encontrados lugares proximos a voce em ${maxDistance} metros.
+      Aumente a area de busca ou cadastre novos lugares!
+      `;
+    }
+  }
+ 
+  loadNearPlaces(maxDistance)
+
+  document.getElementById('nearRange').onchange = async function () {
+    loadNearPlaces();
+  }
 };
+
+
